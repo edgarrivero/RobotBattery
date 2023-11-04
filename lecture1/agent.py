f7f76f7b6e06d9b1f61e0e626ca55d76b1273945
Edgar Rivero <riveroedgar.jm@gmail.com>
2023-11-04 00:35:20 -0500

    QLearning initial

--- lecture1/agent.py
+++ lecture1/agent.py
@@ -1,31 +1,89 @@
+# import numpy as np
+
+
+# class TwoArmedBandit():
+#     def __init__(self, alpha=1):
+#         self.arms = 2
+#         self.alpha = alpha
+#         self.reset()
+
+#     def reset(self):
+#         self.action = 0
+#         self.reward = 0
+#         self.iteration = 0
+#         self.values = np.zeros(self.arms)
+
+#     def update(self, action, reward):
+#         self.action = action
+#         self.reward = reward
+#         self.iteration += 1
+#         self.values[action] = self.values[action] + \
+#             self.alpha * (reward - self.values[action])
+
+#     def get_action(self, mode):
+#         if mode == 'random':
+#             return np.random.choice(self.arms)
+#         elif mode == 'greedy':
+#             return np.argmax(self.values)
+
+#     def render(self):
+#         print("Iteration: {}, Action: {}, Reward: {}, Values: {}".format(
+#             self.iteration, self.action, self.reward, self.values))
+
 import numpy as np
 
 
-class TwoArmedBandit():
-    def __init__(self, alpha=1):
-        self.arms = 2
+class QLearning():
+    def __init__(self, states_n, actions_n, alpha, gamma, epsilon):
+        self.states_n = states_n
+        self.actions_n = actions_n
         self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
         self.reset()
 
     def reset(self):
-        self.action = 0
-        self.reward = 0
+        self.episode = 0
         self.iteration = 0
-        self.values = np.zeros(self.arms)
+        self.state = 0
+        self.action = 0
+        self.next_state = 0
+        self.reward = 0        
+        self.q_table = np.zeros((self.states_n, self.actions_n))
 
-    def update(self, action, reward):
-        self.action = action
-        self.reward = reward
+    def update(self, current_state, action, next_state, reward, terminated):
+        self._update(current_state, action, next_state, reward, terminated)
+        self.q_table[current_state, action] = self.q_table[current_state, action] + self.alpha * \
+            (reward +
+             self.gamma *
+             np.max(self.q_table[next_state]) -
+             self.q_table[current_state, action])
+
+    def _update(self, current_state, action, next_state, reward, terminated):        
         self.iteration += 1
-        self.values[action] = self.values[action] + \
-            self.alpha * (reward - self.values[action])
+        self.state = current_state
+        self.action = action
+        self.next_state = next_state
+        self.reward = reward
+        if terminated:
+            self.episode += 1
+            self.iteration = 0
 
-    def get_action(self, mode):
+    def get_action(self, state, mode):
         if mode == 'random':
-            return np.random.choice(self.arms)
+            return np.random.choice(self.actions_n)
         elif mode == 'greedy':
-            return np.argmax(self.values)
+            return np.argmax(self.q_table[state])
+        elif mode == 'epsilon-greedy':
+            rdm = np.random.uniform(0, 1)
+            if rdm < self.epsilon:
+                return np.random.choice(self.actions_n)
+            else:
+                return np.argmax(self.q_table[state])
 
-    def render(self):
-        print("Iteration: {}, Action: {}, Reward: {}, Values: {}".format(
-            self.iteration, self.action, self.reward, self.values))
+    def render(self, mode='values'):
+        if (mode == 'step'):
+            print("Episode: {}, Iteration: {}, State: {}, Action: {}, Next state: {}, Reward: {}".format(
+                self.episode, self.iteration, self.state, self.action, self.next_state, self.reward))
+        elif (mode == 'values'):
+            print("Q-Table: {}".format(self.q_table))
--- lecture1/main.py
+++ lecture1/main.py
@@ -1,20 +1,69 @@
-import sys
-import gym_environments
+# import sys
+# import gym_environments
+# import gym
+# from agent import TwoArmedBandit
+
+# num_iterations = 100 if len(sys.argv) < 2 else int(sys.argv[1])
+# version = "v0" if len(sys.argv) < 3 else sys.argv[2]
+
+# env = gym.make(f"TwoArmedBandit-{version}")
+# agent = TwoArmedBandit(0.1)
+
+# env.reset(options={'delay': 1})
+
+# for iteration in range(num_iterations):
+#     action = agent.get_action("random")
+#     _, reward, _, _, _ = env.step(action)
+#     agent.update(action, reward)
+#     agent.render()
+
+# env.close()
+
 import gym
-from agent import TwoArmedBandit
+import gym_environments
+from agent import QLearning
 
-num_iterations = 100 if len(sys.argv) < 2 else int(sys.argv[1])
-version = "v0" if len(sys.argv) < 3 else sys.argv[2]
+#RobotBattery-v0, Taxi-v3, FrozenLake-v1, RobotMaze-v0
+ENVIRONMENT = 'RobotBattery-v0'
 
-env = gym.make(f"TwoArmedBandit-{version}")
-agent = TwoArmedBandit(0.1)
+def train(env, agent, episodes):
+    for _ in range(episodes):
+        observation, _ = env.reset()
+        terminated, truncated = False, False
+        while not (terminated or truncated):
+            action = agent.get_action(observation, 'epsilon-greedy')
+            new_observation, reward, terminated, truncated, _ = env.step(
+                action)
+            agent.update(
+                observation,
+                action,
+                new_observation,
+                reward,
+                terminated)
+            observation = new_observation
 
-env.reset(options={'delay': 1})
+def play(env, agent):
+    observation, _ = env.reset()
+    terminated, truncated = False, False
+    while not (terminated or truncated):
+        action = agent.get_action(observation, 'greedy')
+        observation, _, terminated, truncated, _ = env.step(action)
+        env.render()
 
-for iteration in range(num_iterations):
-    action = agent.get_action("random")
-    _, reward, _, _, _ = env.step(action)
-    agent.update(action, reward)
+if __name__ == "__main__":
+
+    env = gym.make(ENVIRONMENT)
+    agent = QLearning(
+        env.observation_space.n, 
+        env.action_space.n, 
+        alpha=0.1, 
+        gamma=0.9, 
+        epsilon=0.1)
+
+    train(env, agent, episodes=10)
     agent.render()
+
+    env = gym.make(ENVIRONMENT, render_mode="human")
+    play(env, agent)
 
 env.close()
