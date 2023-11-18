c334644e812942e43fdbd4ea93462921db1bbd67
Edgar Rivero <riveroedgar.jm@gmail.com>
2023-11-17 19:24:30 -0500

    Environment initial

--- lecture1/environment.py
+++ lecture1/environment.py
@@ -4,16 +4,15 @@ import numpy as np
 class MatematicasEnv(gym.Env):
     def __init__(self):
         super(MatematicasEnv, self).__init__()
-        self.observation_space = gym.spaces.Discrete(4)  # Emociones: feliz, triste, neutro, confundido
-        self.action_space = gym.spaces.Discrete(5)  # Acciones: suma, resta, multiplicacion, division, porcentaje
-        
+        self.observation_space = gym.spaces.Discrete(4)  
+        self.action_space = gym.spaces.Discrete(5)          
         self.current_emotion = None
         self.current_step = 0
         self.max_happiness_category = None  
         self.reset()
 
     def reset(self):
-        self.current_emotion = np.random.choice(self.observation_space.n)  # Emoción aleatoria al inicio
+        self.current_emotion = np.random.choice(self.observation_space.n)  
         self.current_step = 0
         self.max_happiness_category = None 
         return self.current_emotion
@@ -27,57 +26,70 @@ class MatematicasEnv(gym.Env):
         self.current_emotion = np.random.choice(self.observation_space.n)
         self.current_step += 1
         self.max_happiness_category = self.determine_max_happiness_category(categories)
+
+        #Condicionales con las emociones y la accion(categoria) 
+        base = 0.5  
+        emotion_adjustment = 0
+        category_adjustment = 0
+        
+        if self.current_emotion == 0:  
+            emotion_adjustment = 0.2
+        elif self.current_emotion == 1:  
+            emotion_adjustment = 0.1
+        elif self.current_emotion == 2:  
+            emotion_adjustment = -0.1
+        elif self.current_emotion == 3:  
+            emotion_adjustment = -0.2
+
+        
+        if study_category == "suma":  
+            category_adjustment = 0.1
+        elif study_category == "resta":  
+            category_adjustment = 0.1
+        elif study_category == "multiplicacion":  
+            category_adjustment = 0.1
+        elif study_category == "division":  
+            category_adjustment = 0.1
+        elif study_category == "porcentaje":  
+            category_adjustment = 0.1
+
+        if is_correct_answer:  
+            answer_adjustment = emotion_adjustment + category_adjustment
+        else:  
+            answer_adjustment = - (emotion_adjustment + category_adjustment)
+
+        result = base + answer_adjustment
+
+        #probabilidad de correcto
+
+
+        #siguiente emocion
+
         print("Emoción:", self.get_emotion_name(), "Recompensa:", reward, "Categoría de mayor felicidad:", self.get_category_name())
         return self.current_emotion, reward, False, {'max_happiness_category': self.max_happiness_category}
 
-    def calculate_reward(self, study_category, is_correct_answer):
-        base_reward = 0.5  # Valor base para todas las categorías
-        emotion_adjustment = 0
-        category_adjustment = 0
-         # Ajuste de la recompensa según la emoción del estudiante
-        if self.current_emotion == 0:  # Si el estudiante está feliz
-            emotion_adjustment = 0.2
-        elif self.current_emotion == 1:  # Si el estudiante está neutro
-            emotion_adjustment = 0.1
-        elif self.current_emotion == 2:  # Si el estudiante está confundido
-            emotion_adjustment = -0.1
-        elif self.current_emotion == 3:  # Si el estudiante está triste
-            emotion_adjustment = -0.2
-
-        # Ajuste de la recompensa según la categoría de la pregunta
-        if study_category == "suma":  # Si la categoría es suma
-            category_adjustment = 0.1
-        elif study_category == "resta":  # Si la categoría es resta
-            category_adjustment = 0.1
-        elif study_category == "multiplicacion":  # Si la categoría es multiplicación
-            category_adjustment = 0.1
-        elif study_category == "division":  # Si la categoría es división
-            category_adjustment = 0.1
-        elif study_category == "porcentaje":  # Si la categoría es porcentaje
-            category_adjustment = 0.1
-
-        # Ajuste de la recompensa según la respuesta correcta o incorrecta
-        if is_correct_answer:  # Si la respuesta es correcta
-            answer_adjustment = emotion_adjustment + category_adjustment
-        else:  # Si la respuesta es incorrecta
-            answer_adjustment = - (emotion_adjustment + category_adjustment)
-
-        # Cálculo de la recompensa final
-        return base_reward + answer_adjustment
+    def calculate_reward(self):
+        #si responde correcto es 1
+        #caso contrario 0
+        base_reward = 0.5  
+        
+        if self.current_emotion == 1:  
+            base_reward = 1
+        else :
+            base_reward = 0
+        
+        return base_reward
             
     def determine_max_happiness_category(self, categories):
         rewards = {}
-        # Iterar sobre las categorías y calcular la recompensa para cada una
+        
         for category in categories:
             is_correct_answer_Other = np.random.choice([True, False])
             reward = self.calculate_reward(category, is_correct_answer_Other)
-            # Guardar la recompensa en el diccionario con la categoría como clave
             rewards[category] = reward
 
-        # Encontrar la categoría con la mayor recompensa
+        
         max_category = max(rewards, key=rewards.get)
-
-        # Devolver la categoría con la mayor recompensa
         return max_category
 
     def get_emotion_name(self):
--- lecture1/main.py
+++ lecture1/main.py
@@ -5,15 +5,14 @@ from agent import QLearning
 from environment2 import EducationEnv
 from environment import MatematicasEnv
 
-# Crear una instancia del entorno
+
 env = MatematicasEnv()
 
-# Ejemplo de un ciclo de interacción con el entorno
+
 for _ in range(10):
-    action = np.random.randint(4)  # Seleccionar una acción entre 0 y 4
+    action = np.random.randint(4)  
     obs, reward, done, info = env.step(action)
 
-# Reiniciar el entorno
 env.reset()
 
 
